81b5546c8e41d66e3654e4855acd591c6e18131e
Ehimare Agboneni <ehimare.agboneni@gmail.com>
2020-04-10 12:05:03 -0400

    edited fields in entities to have username and password

---
+++ ExpenseReimbursmentSystem/src/main/java/dev/agboneni/entities/Employee.java
@@ -0,0 +1,48 @@
+package dev.agboneni.entities;
+
+public class Employee {
+	
+	private int empId;
+	private String name;
+	private String username;
+	private String password;
+	
+	
+	public Employee(int empId, String name, String username, String password) {
+		this.empId = empId;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", name=" + name + ", username=" + username + ", password=" + password
+				+ "]";
+	}
+	
+
+}
