cd4b50d0dcdae28c33e2f6ca14634a2909a45078
Ehimare Agboneni <ehimare.agboneni@gmail.com>
2020-04-15 08:55:37 -0400

    added DispatcherServlet class

---
+++ ExpenseReimbursementSystemApp/src/main/java/dev/agboneni/entities/Manager.java
@@ -0,0 +1,51 @@
+package dev.agboneni.entities;
+
+public class Manager {
+	private int managerId;
+	private String name;
+	private String username;
+	private String password;
+	
+	
+	public Manager() {
+		super();
+		
+	}
+	public Manager(int managerId, String name, String username, String password) {
+		this.managerId = managerId;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+	}
+	public int getManagerId() {
+		return managerId;
+	}
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Manager [managerId=" + managerId + ", name=" + name + ", username=" + username + ", password="
+				+ password + "]";
+	}
+	
+
+}
---
+++ ExpenseReimbursementSystemApp/src/main/java/dev/agboneni/entities/Reimbursment.java
@@ -0,0 +1,51 @@
+package dev.agboneni.entities;
+
+public class Reimbursment {
+	private double amount;
+	private int empId;
+	private String reason;
+	private String status;
+	
+	
+	public Reimbursment() {
+		super();
+		
+	}
+	public Reimbursment(double amount, int empId, String reason, String status) {
+		this.amount = amount;
+		this.empId = empId;
+		this.reason = reason;
+		this.status = status;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "Reimbursment [amount=" + amount + ", empId=" + empId + ", reason=" + reason + ", status=" + status
+				+ "]";
+	}
+
+	
+}
